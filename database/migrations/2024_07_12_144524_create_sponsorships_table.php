4c4c86e2d0e156485ada46c20c8f30bc51351bc5
LucaElisio <personal.elix@gmail.com>
2024-07-12 16:52:10 +0200

    add: migration sponsorship table

---
+++ database/migrations/2024_07_12_144524_create_sponsorships_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sponsorships', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->enum('type_name', ['Essential', 'Regular', 'Premium']);
+            $table->time('type_duration');
+            $table->decimal('price');
+
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sponsorships');
+    }
+};
