c0f50a99ecfea6bb3fa457b125b5110c5b4667f5
danielemantovani <daniele.mantovani124@gmail.com>
2024-07-15 11:11:23 +0200

    feat: popultated seeder houses table

--- database/seeders/HousesTableSeeder.php
+++ database/seeders/HousesTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\House;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,24 @@ class HousesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $housesArray = config('houses');
+        // dd($housesArray);
+        foreach ($housesArray as $house) {
+            $newHouse = new House();
+            $newHouse->title = $house['title'];
+            $newHouse->price = $house['price'];
+            $newHouse->address = $house['address'];
+            $newHouse->description = $house['description'];
+            $newHouse->rooms = $house['rooms'];
+            $newHouse->beds = $house['beds'];
+            $newHouse->bathrooms = $house['bathrooms'];
+            $newHouse->sqm = $house['sqm'];
+            $newHouse->latitude = $house['latitude'];
+            $newHouse->longitude = $house['longitude'];
+            $newHouse->image = 'https://placehold.co/300x150?text=Anteprima+non+disponibile';
+            $newHouse->visible = $house['visible'];
+            $newHouse->slug = $house['slug'];
+            $newHouse->save();
+        }
     }
 }
