3f4e773eef047a76f24bd8fe133a12533ada6770
Om12ek99 <omar.elkamili@gmail.com>
2024-07-15 15:47:03 +0200

    Merge branch 'create-resource-controller-and-routes'

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\HouseController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,14 +20,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')
+    ->prefix('admin') // prefix of the url of the route
+    ->name('admin.') // prefix of route name
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+        Route::resource('house', HouseController::class);
+    });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
