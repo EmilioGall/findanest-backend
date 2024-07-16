554436d90726ff7756e53cfe91b2544ef152794e
andrea-bertaglia <andreabertaglia@gmail.com>
2024-07-16 15:41:52 +0200

    feat: create page translate to italian

--- resources/views/admin/houses/create.blade.php
+++ resources/views/admin/houses/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <h1>Inserisci un nuovo appartamento</h1>
+        <h1 class="py-2">Inserisci un nuovo appartamento</h1>
 
         {{-- handling dell'errore --}}
         @if ($errors->any())
@@ -22,23 +22,23 @@
 
             <div class="row">
                 <div class="col-12 mb-3">
-                    <label for="title" class="form-label">Title</label>
+                    <label for="title" class="form-label">Nome</label>
                     <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
                 </div>
 
                 <div class="col-12 mb-3">
-                    <label for="address" class="form-label">Address</label>
+                    <label for="address" class="form-label">Indirizzo</label>
                     <input type="text" class="form-control" id="address" name="address" value="{{ old('address') }}">
                 </div>
 
                 <div class="col-12 mb-3">
-                    <label for="description" class="form-label">Description</label>
+                    <label for="description" class="form-label">Descrizione</label>
                     <input type="text" class="form-control" id="description" name="description"
-                           value="{{ old('description') }}">
+                        value="{{ old('description') }}">
                 </div>
 
                 <div class="col-12 mb-3">
-                    <label for="image" class="form-label">Cover Image</label>
+                    <label for="image" class="form-label">Foto</label>
                     <input type="file" class="form-control" id="image" name="image">
                 </div>
             </div>
@@ -46,40 +46,51 @@
             <div class="row">
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label for="rooms" class="form-label">Rooms</label>
-                        <input type="number" class="form-control" id="rooms" name="rooms" value="{{ old('rooms') }}">
+                        <label for="rooms" class="form-label">Stanze</label>
+                        <input type="number" class="form-control" id="rooms" name="rooms"
+                            value="{{ old('rooms') }}">
                     </div>
 
                     <div class="mb-3">
-                        <label for="bathrooms" class="form-label">Bathrooms</label>
-                        <input type="number" class="form-control" id="bathrooms" name="bathrooms" value="{{ old('bathrooms') }}">
+                        <label for="bathrooms" class="form-label">Bagni</label>
+                        <input type="number" class="form-control" id="bathrooms" name="bathrooms"
+                            value="{{ old('bathrooms') }}">
                     </div>
 
                     <div class="mb-3">
-                        <label for="beds" class="form-label">Beds</label>
-                        <input type="number" class="form-control" id="beds" name="beds" value="{{ old('beds') }}">
+                        <label for="beds" class="form-label">Posti Letto</label>
+                        <input type="number" class="form-control" id="beds" name="beds"
+                            value="{{ old('beds') }}">
                     </div>
                 </div>
 
                 <div class="col-md-6">
                     <div class="mb-3">
-                        <label for="price" class="form-label">Price</label>
-                        <input type="number" class="form-control" id="price" name="price" step="0.01" value="{{ old('price') }}">
+                        <label for="price" class="form-label">Prezzo</label>
+                        <input type="number" class="form-control" id="price" name="price" step="0.01"
+                            value="{{ old('price') }}">
                     </div>
 
                     <div class="mb-3">
-                        <label for="sqm" class="form-label">Area (sqm)</label>
-                        <input type="number" class="form-control" id="sqm" name="sqm" value="{{ old('sqm') }}">
+                        <label for="sqm" class="form-label">Dimensione (mq)</label>
+                        <input type="number" class="form-control" id="sqm" name="sqm"
+                            value="{{ old('sqm') }}">
                     </div>
 
-                    <div class="form-check mb-3  mt-5">
-                        <input type="checkbox" class="form-check-input" id="visible" name="visible" {{ old('visible') ? 'checked' : '' }}>
-                        <label class="form-check-label" for="visible">Visible</label>
-                    </div>            
+                    {{-- <div class="form-check mb-3  mt-5">
+                        <input type="checkbox" class="form-check-input" id="visible" name="visible"
+                            {{ old('visible') ? 'checked' : '' }}>
+                        <label class="form-check-label" for="visible">Visibile</label>
+                    </div> --}}
+                    <div class="form-check form-switch mb-3  mt-5">
+                        <input class="form-check-input" type="checkbox" role="switch" class="form-check-input"
+                            id="visible" name="visible" {{ old('visible') ? 'checked' : '' }}>
+                        <label class="form-check-label" for="visible">Visibile</label>
+                    </div>
                 </div>
             </div>
 
-            <button type="submit" class="btn btn-success">Post</button>
+            <button type="submit" class="btn btn-success">Salva</button>
         </form>
     </div>
 @endsection
