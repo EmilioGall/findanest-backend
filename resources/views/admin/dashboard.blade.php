11d33624c558a1a94707385e36a7ba3bbbfd2f68
Om12ek99 <omar.elkamili@gmail.com>
2024-07-16 17:22:22 +0200

    add: modified gradient

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -100,13 +100,14 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    transition: background 0.5s ease, color 0.5s ease, transform 0.3s ease; /* Transizioni per sfondo, colore e trasformazione */
+    transition: background 0.5s ease, color 1s ease, transform 0.3s ease; /* Transizioni per sfondo, colore e trasformazione */
 }
 
 .card:hover {
     
-    
-    color: #5D6D7E ;
+    background: rgb(15,163,177);
+    background: radial-gradient(circle, rgba(15,163,177,0.38139005602240894) 14%, rgba(70,83,98,0.26934523809523814) 100%);
+    color: #333; 
     transform: scale(1.05); 
     box-shadow: 0 4px 20px rgba(0, 0, 0, 0.2); 
 }
