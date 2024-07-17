3c6bbed768c34f3f26d8a8c5b545918a927d523f
Emilio Gallo <emilio.gallo17@gmail.com>
2024-07-17 13:21:45 +0200

    fix: identified and deleted conflicts in View - admin/houses/index.blade

--- resources/views/admin/houses/index.blade.php
+++ resources/views/admin/houses/index.blade.php
@@ -2,86 +2,87 @@
 
 @section('content')
 
-    <div class="container">
+   <div class="container">
 
-        <div class="">
-            <form action="{{ route('search') }}" method="GET"
-                class="form-inline d-flex align-items-center justify-content-between">
-                <input class="form-control mx-2" type="search" name="query" placeholder="Cerca" aria-label="Search">
-                <button class="btn btn-outline-success my-2" type="submit">Cerca</button>
-            </form>
-        </div>
+      <div class="">
+         <form action="{{ route('search') }}" method="GET"
+            class="form-inline d-flex align-items-center justify-content-between">
+            <input class="form-control mx-2"
+               type="search"
+               name="query"
+               placeholder="Cerca"
+               aria-label="Search">
+            <button class="btn btn-outline-success my-2" type="submit">Cerca</button>
+         </form>
+      </div>
 
-        <div class="list-group">
+      <div class="list-group">
 
-            @if ($houses)
-                <table class="table">
+         @if ($houses)
+            <table class="table">
 
-                    <thead>
+               <thead>
 
-                        <tr>
+                  <tr>
 
-                            <th scope="col">#</th>
-                            <th scope="col">Immagine</th>
-                            <th scope="col">Titolo</th>
-                            <th scope="col">Indirizzo</th>
-                            <th scope="col">Prezzo</th>
-                            <th scope="col">Azioni</th>
+                     <th scope="col">#</th>
+                     <th scope="col">Immagine</th>
+                     <th scope="col">Titolo</th>
+                     <th scope="col">Indirizzo</th>
+                     <th scope="col">Prezzo</th>
+                     <th scope="col">Azioni</th>
 
-                        </tr>
+                  </tr>
 
-                    </thead>
+               </thead>
 
-                    <tbody>
-                        @foreach ($houses as $index => $house)
-                            <tr>
+               <tbody>
+                  @foreach ($houses as $index => $house)
+                     <tr>
+
+                        <th scope="row">{{ $index + 1 }}</th>
+
                         <td><img class="w-50" src="{{ asset('storage/' . $house->image) }}" alt=""></td>
 
-                                <th scope="row">{{ $index + 1 }}</th>
+                        <td>{{ $house->title }}</td>
 
-                                <td><img class="w-50" src="{{ asset('storage/' . $house->image) }}" alt=""></td>
+                        <td>{{ $house->address }}</td>
 
+                        <td>{{ $house->price }} €/notte</td>
 
-                                <td>{{ $house->title }}</td>
+                        <td>
 
-                                <td>{{ $house->address }}</td>
+                           <div class="d-flex gap-2">
 
-                                <td>{{ $house->price }} €/notte</td>
-                                <a class="btn btn-warning text-white" href="{{ route('admin.house.edit', ['house' => $house->slug]) }}"><i class="fa-solid fa-pencil"></i></a>
+                              <a class="btn btn-primary"
+                                 href="{{ route('admin.house.show', ['house' => $house->slug]) }}"><i
+                                    class="fa-solid fa-eye"></i></a>
 
-                                <td>
+                              <a class="btn btn-warning text-white" href=""><i class="fa-solid fa-pencil"></i></a>
 
-                                    <div class="d-flex gap-2">
+                              <a class="btn btn-danger" href=""><i class="fa-solid fa-trash"></i></a>
 
-                                        <a class="btn btn-primary"
-                                            href="{{ route('admin.house.show', ['house' => $house->slug]) }}"><i
-                                                class="fa-solid fa-eye"></i></a>
+                           </div>
 
-                                        <a class="btn btn-warning text-white" href=""><i
-                                                class="fa-solid fa-pencil"></i></a>
+                        </td>
 
-                                        <a class="btn btn-danger" href=""><i class="fa-solid fa-trash"></i></a>
+                     </tr>
+                  @endforeach
+               </tbody>
 
-                                    </div>
+            </table>
+         @else
+            <h1>Nessun risultato</h1>
+         @endif
 
-                                </td>
+      </div>
 
-                            </tr>
-                        @endforeach
-                    </tbody>
+   </div>
 
-                </table>
-            @else
-                <h1>Nessun risultato</h1>
-            @endif
-
-        </div>
-    </div>
-
-    <style>
-        img {
-            height: 50px;
-            object-fit: cover;
-        }
-    </style>
+   <style>
+      img {
+         height: 50px;
+         object-fit: cover;
+      }
+   </style>
 @endsection
