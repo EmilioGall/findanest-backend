68429943640a7fc053cc1cd8e6e00b3a9d0e7e4b
danielemantovani <daniele.mantovani124@gmail.com>
2024-07-17 10:42:10 +0200

    feat: create file auth in resource for messagge error in ita e modify language in app.php 'locale'

---
+++ resources/lang/it/auth.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'failed' => 'Le credenziali non sono corrette.',
+    'password' => 'La password inserita non è corretta.',
+    'throttle' => 'Troppi tentativi di login. Riprova tra :seconds secondi.',
+];
