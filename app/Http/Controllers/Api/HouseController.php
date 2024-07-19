83cd6b41528c9c9c9b2325a2ebc2eb18b86eb848
Om12ek99 <omar.elkamili@gmail.com>
2024-07-19 22:59:27 +0200

    add: api now contains all homes

--- app/Http/Controllers/Api/HouseController.php
+++ app/Http/Controllers/Api/HouseController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\House;
 use Illuminate\Http\Request;
 
 class HouseController extends Controller
 {
     public function index(){
+        $houses = House::all();
         $data = [
-            'result' => 'success'
+
+            'result' => $houses,
+            'success' => true
         ];
         return response()->json($data);
     }
