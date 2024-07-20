48e5a2e36ce3e93b88e3668fd18ab03351404531
Om12ek99 <omar.elkamili@gmail.com>
2024-07-21 00:01:07 +0200

    add: add jubotrone to welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,37 @@
    <div class="jumbotron p-5 mb-4 rounded-3">
 
       <div class="container py-3">
+         <div class="row align-items-center py-4 g-5">
+            <div class="col-12 col-md-6">
+              <div class="text-center text-md-start">
+                <h1 class="display-md-2 display-4 fw-bold text-dark pb-2">
+                  <span class="text-primary">Affitta con facilità </span>guadagna senza pensieri!
+                </h1>
+                <p class="lead">
+                  Benvenuto nel tuo nuovo nido accogliente, il luogo ideale per
+               trasformare ogni soggiorno in un'esperienza unica e indimenticabile. Con le nostre soluzioni per affitti
+               brevi, troverai il massimo del comfort e della comodità, che tu sia in viaggio per lavoro o per piacere.
+               Scopri la nostra ampia gamma di opzioni e lasciati coccolare dalla nostra ospitalità.
+                </p>
+                <button
+                  class="btn btn-primary px-5 py-3 mt-3 fs-5 fw-medium"
+                  type="button"
+                >
+                  Pubblica Il Tuo Primo Annuncio
+                </button>
+              </div>
+            </div>
+            <div class="col-12 col-md-6">
+               <img src="{{ asset('images/jumbo.jpg') }}"  alt="logo findanest">
+            </div>
+          </div>
 
          <div class="logo text-center">
 
-            <img src="{{ asset('images/logo/findnest_logo_green_v1.svg') }}" class="w-25" alt="logo findanest">
+           
 
-            <p class="py-2 mt-5 fs-4 lh-lg text-secondary">Benvenuto nel tuo nuovo nido accogliente, il luogo ideale per
-               trasformare ogni soggiorno in un'esperienza unica e indimenticabile. Con le nostre soluzioni per affitti
-               brevi, troverai il massimo del comfort e della comodità, che tu sia in viaggio per lavoro o per piacere.
-               Scopri la nostra ampia gamma di opzioni e lasciati coccolare dalla nostra ospitalità.</p>
-
+            
+               
          </div>
 
       </div>
