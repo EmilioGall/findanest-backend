c107c74419911b85690860003f154eecafd6814b
Emilio Gallo <emilio.gallo17@gmail.com>
2024-07-20 21:41:32 +0200

    feat: moved colors variables in file  .env & adjusted Views - welcome.blade & layout/app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,97 +1,136 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+      <!-- CSRF Token -->
+      <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Find-A-Nest') }}</title>
+      <title>{{ config('app.name', 'Find-A-Nest') }}</title>
 
-    {{-- Favicon --}}
-    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
+      {{-- Favicon --}}
+      <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+      <!-- Fonts -->
+      <link rel="dns-prefetch" href="//fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Usando Vite -->
-    @vite(['resources/js/app.js'])
-</head>
+      <!-- Usando Vite -->
+      @vite(['resources/js/app.js'])
+   </head>
 
-<body>
-    <div id="app">
+   <body>
 
+      <div id="app">
 
-        <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color: #64afdc">
+         <nav class="navbar navbar-expand-md navbar-light shadow-sm">
+
             <div class="container">
-                <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
-                    <div class="logo">
-                        <img src="{{ asset('images/findanest-logo-h1.svg') }}" style="width: 150px; object-fit: contain"
-                            alt="">
-                    </div>
-                    {{-- config('app.name', 'Laravel') --}}
-                </a>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+               <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}">
+                  <div class="logo">
+                     <img src="{{ asset('images/logo/findnest_logo_green_h1.svg') }}" style="width: 150px; object-fit: contain"
+                        alt="">
+                  </div>
+                  {{-- config('app.name', 'Laravel') --}}
+               </a>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
+               <button class="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="{{ __('Toggle navigation') }}">
+
+                  <span class="navbar-toggler-icon"></span>
+
+               </button>
+
+               <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                  <!-- Left Side Of Navbar -->
+                  <ul class="navbar-nav me-auto">
+                     <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
+                     </li>
+                  </ul>
+
+                  <!-- Right Side Of Navbar -->
+                  <ul class="navbar-nav ml-auto">
+
+                     <!-- Authentication Links -->
+                     @guest
+
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/') }}">{{ __('Home') }}</a>
+                           <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                         </li>
-                    </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrati') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+                        @if (Route::has('register'))
+                           <li class="nav-item">
+                              <a class="nav-link" href="{{ route('register') }}">{{ __('Registrati') }}</a>
+                           </li>
+                        @endif
+                     @else
+                        <li class="nav-item dropdown">
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ url('admin') }}">{{ __('Dashboard') }}</a>
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
+                           <a id="navbarDropdown"
+                              class="nav-link dropdown-toggle"
+                              href="#"
+                              role="button"
+                              data-bs-toggle="dropdown"
+                              aria-haspopup="true"
+                              aria-expanded="false"
+                              v-pre>
+                              {{ Auth::user()->name }}
+                           </a>
+
+                           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+
+                              <a class="dropdown-item" href="{{ url('admin') }}">{{ __('Dashboard') }}</a>
+
+                              <a class="dropdown-item" href="{{ route('logout') }}"
+                                 onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                 {{ __('Logout') }}
+                              </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+                              <form id="logout-form"
+                                 action="{{ route('logout') }}"
+                                 method="POST"
+                                 class="d-none">
+                                 @csrf
+                              </form>
+
+                           </div>
+
+                        </li>
+
+                     @endguest
+
+                  </ul>
+
+               </div>
+
             </div>
-        </nav>
 
-        <main class="">
+         </nav>
+
+         <main class="">
             @yield('content')
-        </main>
+         </main>
 
-    </div>
-</body>
+      </div>
+   </body>
 
 </html>
+
+<style>
+
+    nav {
+        background-color: {{ env('color_light_grey') }};
+
+    }
+</style>
