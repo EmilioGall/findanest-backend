5f0b8c3d53d06835ea7db91e57e0e3524827fd41
Emilio Gallo <emilio.gallo17@gmail.com>
2024-07-21 16:20:24 +0200

    fix: resolved conflicts in View - admin/leads/index.blade

--- resources/views/admin/leads/index.blade.php
+++ resources/views/admin/leads/index.blade.php
@@ -1,68 +1,53 @@
 @extends('layouts.admin')
 
 @php
-    use Carbon\Carbon;
+   use Carbon\Carbon;
 @endphp
 
 
 @section('content')
-    <div class="container">
-        <h1 class="py-3 mb-2">Richieste di contatto</h1>
+   <div class="container">
 
-        <form method="GET" action="{{ route('admin.leads.index') }}" class="pb-4">
-            <div class="col-4">
-                <div class="mb-3">
-                    <label for="house_id" class="form-label"><small>Seleziona la casa per visualizzare i
-                            messaggi</small></label>
-                    <select class="form-select" id="house_id" name="house_id" onchange="this.form.submit()">
-                        <option value="">Tutte le case</option>
-                        @foreach ($houses as $house)
-                            <option value="{{ $house->id }}" {{ $house->id == $houseId ? 'selected' : '' }}>
-                                {{ $house->title }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
+      <h1 class="py-3 mb-2">Richieste di contatto</h1>
+
+      <form method="GET" action="{{ route('admin.leads.index') }}" class="pb-4">
+         <div class="col-4">
+            <div class="mb-3">
+               <label for="house_id" class="form-label"><small>Seleziona la casa per visualizzare i
+                     messaggi</small></label>
+               <select class="form-select"
+                  id="house_id"
+                  name="house_id"
+                  onchange="this.form.submit()">
+                  <option value="">Tutte le case</option>
+                  @foreach ($houses as $house)
+                     <option value="{{ $house->id }}" {{ $house->id == $houseId ? 'selected' : '' }}>
+                        {{ $house->title }}
+                     </option>
+                  @endforeach
+               </select>
             </div>
+         </div>
 
-        </form>
-        <h1 class="py-3 mb-2">Richieste di contatto</h1>
+      </form>
 
-        <form method="GET" action="{{ route('admin.leads.index') }}" class="pb-4">
-            <div class="col-4">
-                <div class="mb-3">
-                    <label for="house_id" class="form-label"><small>Seleziona la casa per visualizzare i
-                            messaggi</small></label>
-                    <select class="form-select" id="house_id" name="house_id" onchange="this.form.submit()">
-                        <option value="">Tutte le case</option>
-                        @foreach ($houses as $house)
-                            <option value="{{ $house->id }}" {{ $house->id == $houseId ? 'selected' : '' }}>
-                                {{ $house->title }}
-                            </option>
-                        @endforeach
-                    </select>
-                </div>
-            </div>
-
-        </form>
-
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    {{-- <th scope="col">Stato</th> --}}
-                    <th scope="col">Nome</th>
-                    <th scope="col">E-Mail</th>
-                    <th scope="col">Telefono</th>
-                    <th scope="col">Ricevuto il</th>
-                    <th scope="col">Azioni</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($leads as $index => $lead)
-                    <tr>
-                        <th scope="row">{{ $index + 1 }}</th>
-                        {{-- <td class="col-auto">
+      <table class="table">
+         <thead>
+            <tr>
+               <th scope="col">#</th>
+               {{-- <th scope="col">Stato</th> --}}
+               <th scope="col">Nome</th>
+               <th scope="col">E-Mail</th>
+               <th scope="col">Telefono</th>
+               <th scope="col">Ricevuto il</th>
+               <th scope="col">Azioni</th>
+            </tr>
+         </thead>
+         <tbody>
+            @foreach ($leads as $index => $lead)
+               <tr>
+                  <th scope="row">{{ $index + 1 }}</th>
+                  {{-- <td class="col-auto">
                             <select
                                 class="form-select form-select-sm badge rounded-pill {{ $lead->status == 'closed' ? 'bg-warning' : ($lead->status == 'handled' ? 'bg-warning' : 'bg-success') }}"
                                 id="autoSizingSelect">
@@ -72,24 +57,38 @@
                             </select>
                         </td> --}}
 
-                        <td>{{ $lead->name }}</td>
-                        <td>{{ $lead->email }}</td>
-                        <td>{{ $lead->phone_number }}</td>
-                        <td>{{ Carbon::parse($lead->created_at)->format('d/m/Y H:i') }}</td>
-                        <td>
-                            <div class="d-flex gap-2">
-                                <a class="btn btn-sm btn-outline-primary"
-                                    href="{{ route('admin.leads.show', ['lead' => $lead->id]) }}">
-                                    <span>Dettagli</span>
-                                </a>
-                            </div>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-        <div>
-            {{ $leads->links() }}
-        </div>
-    </div>
+                  <td>{{ $lead->name }}</td>
+                  <td>{{ $lead->email }}</td>
+                  <td>{{ $lead->phone_number }}</td>
+                  <td>{{ Carbon::parse($lead->created_at)->format('d/m/Y H:i') }}</td>
+                  <td>
+                     <div class="d-flex gap-2">
+                        <a class="btn btn-sm btn-details"
+                           href="{{ route('admin.leads.show', ['lead' => $lead->id]) }}">
+                           <span>Dettagli</span>
+                        </a>
+                     </div>
+                  </td>
+               </tr>
+            @endforeach
+         </tbody>
+      </table>
+      <div>
+         {{ $leads->links() }}
+      </div>
+   </div>
+
+   <style>
+      .btn-details {
+         height: 50%;
+         border-color: {{ env('color_light_purple') }};
+         color: {{ env('color_light_purple') }};
+
+         &:hover {
+            color: white;
+            box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.2);
+            background-color: {{ env('color_light_purple') }};
+         }
+      }
+   </style>
 @endsection
