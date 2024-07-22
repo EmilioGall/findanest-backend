75808cfb26b3db5538b354ed8487ddb6452522f3
danielemantovani <daniele.mantovani124@gmail.com>
2024-07-22 14:09:42 +0200

    feat: add surname and date of birth in pofile section

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -12,12 +12,14 @@
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 bg-white border-b border-gray-200">
                 <h3 class="text-lg font-semibold mb-4 mt-5">Il tuo profilo</h3>
-                <p><strong>Nome:</strong> {{ $user->name }}</p>
-                {{-- <p><strong>Cognome:</strong> {{ $user->surname }}</p> --}}
-                <p><strong>Email:</strong> {{ $user->email }}</p>
+                <p><span class="fw-bold">Nome:</span> {{ $user->name }}</p>
+                <p><span class="fw-bold">Cognome:</span> {{ $user->surname }}</p>
+                <p><span class="fw-bold">Data di nascita:</span> {{ $user->date_of_birth->format('d-m-Y') }}</p>
+                <p><span class="fw-bold">Email:</span> {{ $user->email }}</p>
             </div>
         </div>
     </div>
 </div>
+
 @endsection
 
