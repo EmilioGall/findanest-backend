9bec1782a6749d23f782b103ece90ffd3b6e9db0
andrea-bertaglia <andreabertaglia@gmail.com>
2024-07-24 18:00:20 +0200

    feat: add payment function

--- routes/web.php
+++ routes/web.php
@@ -58,5 +58,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
 });
 
+// search
+Route::get('/search', [HouseController::class, 'search'])->name('search');
 
 require __DIR__ . '/auth.php';
