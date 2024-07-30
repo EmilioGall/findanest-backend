57117981f4208d23f31c0acae91cd81435b1cdb7
andrea-bertaglia <andreabertaglia@gmail.com>
2024-07-30 15:23:01 +0200

    fix sponsorships seeder

--- database/seeders/SponsorshipTableSeeder.php
+++ database/seeders/SponsorshipTableSeeder.php
@@ -27,7 +27,7 @@ class SponsorshipTableSeeder extends Seeder
         }
 
         // Ottenere le case con ID da 1 a 10
-        $houses = House::whereBetween('id', [1, 5])->get();
+        $houses = House::whereBetween('id', [1, 6])->get();
 
         // Assegna una sponsorizzazione a ciascuna casa
         foreach ($houses as $house) {
